4af0aae4ad3fe1575c92df95117ede0ee77e12e0
rocio losasso <rociolosasso@gmail.com>
2023-05-15 10:31:33 -0300

    correccion de url y dockerfile

--- src/main/java/com/portfolio/rbl/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/rbl/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin (origins = "https://portfolio-d5aa1.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
